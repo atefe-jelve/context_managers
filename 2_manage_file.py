a6e40fa3a1bd3d7732a15e56a2a6f31ff353fc1e
atefe jelve <a.jelve.69@gmail.com>
2025-05-13 11:44:52 +0330

    add context manager for file

--- 2_manage_file.py
+++ 2_manage_file.py
@@ -1,15 +1,31 @@
+""" 
+    in this part we impelement a file manager for open and close our file
+    in __exit__ the reason for return True is that for instance is , 
+    we call a method that we dosent that the error python dosent show and we can handle it.
 
-
-
+"""
 class ManagerFile:
     
-    def __init__(self):
-        pass
-
-    def __exit__(self):
-        pass
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None  # keep our conecction here for file
 
     def __enter__(self):
-        pass
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.file.close()
+        print(exc_type)
+        print(exc_value)
+        print(exc_tb)
+        return True  # diactive errors in __exit__
+
+
+with ManagerFile('file.txt', 'r') as f:
+    for i in f:
+        print(i)
+
 
 
